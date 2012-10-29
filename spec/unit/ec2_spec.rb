8fbb374c826b234c59f41817b8150378e14a1524
Jeff McCune <jeff@puppetlabs.com>
2012-10-29 16:38:28 -0700

    WIP - Refactor the EC2 facts
    Conflicts:
    	lib/facter/ec2.rb
    	lib/facter/util/ec2.rb
    	spec/unit/ec2_spec.rb

--- spec/unit/ec2_spec.rb
+++ spec/unit/ec2_spec.rb
@@ -8,33 +8,38 @@ describe "ec2 facts" do
   # environments.
   let(:api_prefix) { "http://169.254.169.254" }
 
+  subject do
+    Facter::Util::EC2.new
+  end
+
+  before :each do
+    # Prevent loading the fact file from trying to talk to the EC2 API.
+    # We explicitly call addfacts in the examples.
+    Facter::Util::EC2.stubs(:addfacts)
+  end
+
   describe "when running on ec2" do
     before :each do
-      # This is an ec2 instance, not a eucalyptus instance
-      Facter::Util::EC2.stubs(:has_euca_mac?).returns(false)
-      Facter::Util::EC2.stubs(:has_openstack_mac?).returns(false)
-      Facter::Util::EC2.stubs(:has_ec2_arp?).returns(true)
-
-      # Assume we can connect
-      Facter::Util::EC2.stubs(:can_connect?).returns(true)
+      subject.stubs(:has_ec2_arp?).returns(true)
+      subject.stubs(:can_connect?).returns(true)
+      subject.stubs(:has_euca_mac?).returns(false)
+      subject.stubs(:has_openstack_mac?).returns(false)
     end
 
-    it "should create flat meta-data facts" do
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/").
-        at_least_once.returns(StringIO.new("foo"))
+    describe "automatically generated facts" do
+      it "should create flat meta-data facts" do
+        subject.stubs(:open).
+          with("#{api_prefix}/2008-02-01/meta-data/").at_least_once.
+          returns(StringIO.new("user_defined_key"))
+        subject.stubs(:open).
+          with("#{api_prefix}/2008-02-01/meta-data/user_defined_key").at_least_once.
+          returns(StringIO.new("user_defined_value"))
 
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/meta-data/foo").
-        at_least_once.returns(StringIO.new("bar"))
+        subject.stubs(:add_userdata_facts)
 
-      # No user-data
-      Object.any_instance.expects(:open).
-        with("#{api_prefix}/2008-02-01/user-data/").
-        at_least_once.returns(StringIO.new(""))
-
-      Facter.collection.internal_loader.load(:ec2)
-      Facter.fact(:ec2_foo).value.should == "bar"
+        Facter.collection.loader.load(:ec2)
+        Facter.fact(:ec2_user_defined_key).value.should == "user_defined_value"
+      end
     end
 
     it "should create flat meta-data facts with comma seperation" do
